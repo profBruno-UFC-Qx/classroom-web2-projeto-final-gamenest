5df0ce69ebc640e0ea14ef78640a649e07c78573
Iarley Souza <souzaiarley20@gmail.com>
2025-12-30 21:57:35 -0300

    feat: adiciona front do GameNest

---
+++ frontend/src/api/index.ts
@@ -0,0 +1,8 @@
+import axios from "axios";
+
+export const BASE_URL = 'https://gamenest-back.onrender.com'
+
+export const api = axios.create({
+    baseURL: `${BASE_URL}/api`,
+    timeout: 3000
+});
